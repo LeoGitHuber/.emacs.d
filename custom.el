29a8fb653a2b33f49f04f612f86390e3cbb4820a
LeoGitHuber <lkhjoker@163.com>
2023-02-23 09:16:49 +0800

    Huge Change && Mode-Line-format

---
+++ custom.el
@@ -0,0 +1,21 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   '("bb08c73af94ee74453c90422485b29e5643b73b05e8de029a6909af6a3fb3f58" "1b8d67b43ff1723960eb5e0cba512a2c7a2ad544ddb2533a90101fd1852b426e" "628278136f88aa1a151bb2d6c8a86bf2b7631fbea5f0f76cba2a0079cd910f7d" "06f0b439b62164c6f8f84fdda32b62fb50b6d00e8b01c2208e55543a6337433a" "251ed7ecd97af314cd77b07359a09da12dcd97be35e3ab761d4a92d8d8cf9a71" "ce31c9ebe566a32f659f9d1ba34ddede17270bad3e21d6eb92ec79fc4d2b2d44" default))
+ '(package-selected-packages
+   '(ccls color-theme-modern rainbow-mode company-box lsp-ui mwim browse-kill-ring aggressive-indent calibredb winum dirvish hide-mode-line multiple-cursors color-theme-sanityinc-tomorrow benchmark-init org-contrib valign visual-fill-column helpful ef-themes org-roam-ui org-roam rust-mode treemacs all-the-icons which-key eglot yasnippet pyim-wbdict ##)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(default ((t nil)))
+ '(fixed-pitch ((t (:family "Essential PragmataPro" :height 1.0))))
+ '(org-src ((t (:inherit fixed-pitch))))
+ '(org-table ((t (:inherit fixed-pitch))))
+ '(org-tag ((t (:inherit fixed-pitch))))
+ '(org-verbatim ((t (:inherit fixed-pitch))))
+ '(variable-pitch ((t (:family "Bookerly" :height 1.0)))))
--- early-init.el
+++ early-init.el
@@ -1,5 +1,8 @@
+
 (add-to-list 'default-frame-alist '(menu-bar-lines . 0))
 (add-to-list 'default-frame-alist '(tool-bar-lines . 0))
 (scroll-bar-mode -1)
 (add-to-list 'default-frame-alist '(fullscreen . maximized))
 ;; (load-theme 'modus-operandi t)
+
+(setq gc-cons-threshold most-positive-fixnum)
